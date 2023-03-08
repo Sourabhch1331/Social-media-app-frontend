8cc3eeb0c0d5933f36025e4dc4bcd90cc4664e87
Sourabh Choudhary <Sourabhch1331@gmail.com>
2023-03-08 10:53:09 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-unused-vars */
+import {createContext,useState,useEffect} from 'react';
+import {Route,Routes} from 'react-router-dom';
+
+import './css-modules/App.module.css';
+import Header from './components/Header';
+
+const userContext = createContext({user:null,isLogedIn:false});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [userDetails,setUserDetails] = useState(null);
+	const [isLogedIn,setIsLogedIn] = useState(false);
+	const [err,setErr] = useState(null);
+	const [isPending,setIsPending] = useState(true);
+
+	
+
+	useEffect(()=>{
+		const controller = new AbortController();
+		const signal = controller.signal;
+
+
+		const getUser= async ()=>{
+			const url=`https://wild-gray-parrot-gear.cyclic.app/api/v1/me`;
+				fetch(url, {
+					method: 'GET',
+					signal
+				})
+				.then(res => res.json())
+				.then(res => console.log(res))
+				.catch(err=>{
+					if(err.name !== 'AbortError'){
+						console.log("Error 💥",err);
+					}
+				});
+		}
+		
+		// getUser();
+
+		return ()=>{
+			controller.abort();
+		}
+
+	},[]);
+
+	return (
+		<div className="App">
+			<userContext.Provider value={userDetails}>
+				<Header/>
+				{/* <NavBar/> */}
+				<div>side bar</div>
+				<Routes>
+					<Route path='/login' element={<h1>login page</h1>} />
+				</Routes>
+			</userContext.Provider>
+			</div>
+
+	);
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+import {Link} from 'react-router-dom';
+import { useContext } from 'react';
+import style from '../css-modules/Header.module.css';
+
+const Header = ()=>{
+    const userDetails = useContext(userContext);
+    
+    return (
+        <header className={style.navBar}>
+            <span></span>
+            <h2>Socionect</h2>
+            <ul>
+                <li><Link to='/login'>login</Link></li>
+                <li><Link to='/signup'>signup</Link></li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header;
